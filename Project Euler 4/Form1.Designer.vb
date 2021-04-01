a951be630ab972e6f4e5320e917c4c2da1b766e9
Jeremy Harding <75227382+whatdoesthismean@users.noreply.github.com>
2021-03-31 17:09:10 -0700

    Add project files.

---
+++ Project Euler 4/Form1.Designer.vb
@@ -0,0 +1,77 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.lstResult = New System.Windows.Forms.ListBox()
+        Me.lblHighest = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'lstResult
+        '
+        Me.lstResult.FormattingEnabled = True
+        Me.lstResult.ItemHeight = 15
+        Me.lstResult.Location = New System.Drawing.Point(93, 12)
+        Me.lstResult.MultiColumn = True
+        Me.lstResult.Name = "lstResult"
+        Me.lstResult.Size = New System.Drawing.Size(695, 424)
+        Me.lstResult.Sorted = True
+        Me.lstResult.TabIndex = 1
+        '
+        'lblHighest
+        '
+        Me.lblHighest.AutoSize = True
+        Me.lblHighest.Location = New System.Drawing.Point(12, 198)
+        Me.lblHighest.Name = "lblHighest"
+        Me.lblHighest.Size = New System.Drawing.Size(41, 15)
+        Me.lblHighest.TabIndex = 2
+        Me.lblHighest.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblHighest)
+        Me.Controls.Add(Me.lstResult)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents lstResult As ListBox
+    Friend WithEvents lblHighest As Label
+End Class
---
+++ Project Euler 4/Form1.vb
@@ -0,0 +1,51 @@
+﻿Public Class Form1
+    '********************
+    '*   project euler  *
+    '*     problem 4    *
+    '*     solved by    *
+    '* WhatDoesThisMean *
+    '*    03/31/2021    *
+    '********************
+    '*   you can solve  *
+    '*      it too      *
+    '* projecteuler.net *
+    '********************
+    'Problem 4 is to find the highest palindromic result when multiplying two three-digit numbers together.
+
+    Dim intNum1 As Integer = 100 'first three digit number
+    Dim intNum2 As Integer = 100 'second three digit number
+    Dim intCount As Integer = 0 'counts the number of items in the results list
+    Dim intResult As Integer 'holds the result of each round of multiplication
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        For intNum2 = 100 To 999
+
+            'the inner for next loop does the multiplication and counts intNum1 through all the numbers from 100 - 999
+            For intNum1 = 100 To 999
+
+                'multiply intNum1 by intNum2
+                intResult = intNum1 * intNum2
+
+                'Weed out the non-palindromic results and save the palindromic results in a list.
+                'The palindromes are found by comparing the elements in the string First:Last then by index of the elements in the string 1:4 2:3
+                'Results with a length less than 6 are also weeded out, since we are looking for the highest palindromic result, and there are some palindromic results with 6 digits.
+                If intResult.ToString.Length = 6 And intResult.ToString.First = intResult.ToString.Last And intResult.ToString.ElementAt(1) = intResult.ToString.ElementAt(4) And intResult.ToString.ElementAt(2) = intResult.ToString.ElementAt(3) Then
+                    lstResult.Items.Add(intResult.ToString)
+                End If
+
+            Next
+
+            'outer for next loop runs once everytime the inner for next loop finishes a complete cycle. It counts intNum2 through all the  numbers from 100 - 999
+
+        Next
+
+        'Display the answer to the challenge by taking the last item in the (sorted=true) list and putting it in a label.
+
+        intCount = lstResult.Items.Count - 1
+
+        lblHighest.Text = lstResult.Items(intCount)
+
+    End Sub
+End Class
